9e746ff8708d42e7fb2c9a283bfc9b0d51bf3ba6
Simon Garber <Simon.garber@gmail.com>
2019-07-04 15:24:12 -0400

    added the functionality that allows for urls to be passed into the command line return a parsed json object if there are no errors. The cat breed is the variable that is passed into the command line which has to match a breed that is in the object otherwise an error is thrown.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,31 @@
 const request = require('request');
+const breedChoice = process.argv[2];
+const url = `htt_ps://api.thecatapi.com/v1/breeds/search?q=${breedChoice}`;
+
+const get = function(url,callback) {
+  request.get(url,function(err,resp,body) {
+        
+        
+        
+    if (err) {
+      throw "error getting URL" + err;
+
+    } else {
+      const data = JSON.parse(body);
+        
+      if (data.length === 0) {
+            
+        throw "Breed not found, try again..";
+      }
+      callback(data);
+
+    }
+  });
+};
+
+get(url,(elem)=>{
+  console.log(elem);
+});
+//anonymous function
+
+
